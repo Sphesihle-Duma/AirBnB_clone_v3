3a710b44283d4b03d6382ccb989954c574144553
WiltonMashele <Wiltonmashele@gmail.com>
2023-10-29 10:25:43 -0700

    updating

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,16 +74,3 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-
-    def get(self, cls, id):
-        """retrieve one object"""
-        if cls in name2class and isinstance(id, str):
-            return self.__session.query(name2class[cls]).filter_by(id=id).first()
-        return None
-
-    def count(self, cls=None):
-        """count the number of objects in storage"""
-        if cls:
-            return self.__session.query(name2class.get(cls, None)).count()
-        else:
-            return sum(self.__session.query(cls).count() for cls in name2class.values())
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,16 +68,3 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-
-    def get(self, cls, id):
-        """Retrieve an object"""
-        if isinstance(cls, str) and isinstance(id, str) and cls in classes:
-            return self.__objects.get(f"{cls}.{id}", None)
-        return None
-
-    def count(self, cls=None):
-        """Count number of objects in storage"""
-        if isinstance(cls, str) and cls in classes:
-            return len(self.all(cls))
-        return len(self.__objects) if cls is None else 0
-
