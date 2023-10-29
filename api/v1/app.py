74e525b386f6c801783073ad2ecc7b5b9e844886
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-10-29 14:19:34 -0700

    fixing pycodestyle in app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,18 +6,22 @@ from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
 
+
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(app_views)
     cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
-    def teardown_appcontext(error):
-        """Function to be called when the application context is torn down"""
-        storage.close()
 
-    def page_not_found(error):
-        """Handle 404 Not Found errors"""
-        return make_response(jsonify({'error': 'Not found'}), 404)
+def teardown_appcontext(error):
+    """Function to be called when the application context is torn down"""
+    storage.close()
+
+
+def page_not_found(error):
+    """Handle 404 Not Found errors"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == "__main__":
     app = create_app()
