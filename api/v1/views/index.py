dcf28a2d0c73d67788fa0aed689e804140ec9df6
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-10-30 10:52:48 -0700

    Status of API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,8 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
+@app_views.route("/status", strict_slashes=False, methods=["GET"])
 def hbnbStatus():
     """hbnbStatus"""
     return jsonify({"status": "OK"})
-
-if __name__ == "__main__":
-    app_views.run(host='0.0.0.0', port=5000)
