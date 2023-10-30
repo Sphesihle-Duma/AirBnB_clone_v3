11eebac56c75de8759d30adeace06742f27eb82c
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-10-30 13:16:50 -0700

    handling the 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,17 +10,17 @@ def create_app():
     """ Creating the flask app """
     app = Flask(__name__)
     app.register_blueprint(app_views)
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        """Handle 404 Not Found errors"""
+        return make_response(jsonify({'error': 'Not found'}), 404)
     return app
 
 
 def teardown_appcontext(error):
     """Function to be called when the application context is torn down"""
     storage.close()
-
-
-def page_not_found(error):
-    """Handle 404 Not Found errors"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
